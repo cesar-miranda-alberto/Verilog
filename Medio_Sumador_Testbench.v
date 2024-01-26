475ba556787dcd4fa838032f3d80b60d5d23ef8a
cesar-miranda-alberto <142947777+cesar-miranda-alberto@users.noreply.github.com>
2024-01-26 14:51:40 -0600

    Medio_Sumador_Testbench.v

--- Medio_Sumador_Testbench.v
+++ Medio_Sumador_Testbench.v
@@ -7,23 +7,23 @@
 		Veersión: 1.0
 */
 
-`timescale 1ns/1ps													  // Establece escala de tiempo para la simulación
+`timescale 1ns/1ps													  	  // Establece escala de tiempo para la simulación
 
-module Medio_Sumador_Testbench();									  // Crea y Asigna nombre al módulo 
+module Medio_Sumador_Testbench();									  			  // Crea y Asigna nombre al módulo 
 	reg A,B;														  // Crean las variables de entrada de tipo reg
 	wire Sa,Cs;														  // Crean las variables de salida de tipo wire
 	
-	Medio_Sumador_Testbench tbms(A,B,Sa,Cs);						  // Instanciación del módulo medio sumador con sus respectivas variables
+	Medio_Sumador_Testbench tbms(A,B,Sa,Cs);						 				  // Instanciación del módulo medio sumador con sus respectivas variables
 	
 	initial 														  // Comienzo
 	begin												
 		
-		      A = 1'b0; B = 1'b0;									  // Asigna valores a las variables de entrada
-       #100    A = 1'b0; B = 1'b1;									  // Asigna valores a las variables de entrada con un retardo de 100
+		      A = 1'b0; B = 1'b0;											  // Asigna valores a las variables de entrada
+       #100    A = 1'b0; B = 1'b1;									  			  // Asigna valores a las variables de entrada con un retardo de 100
        #100    A = 1'b1; B = 1'b0;
        #100    A = 1'b1; B = 1'b1; 
 	   
-	end																  // Fin
+	end															  // Fin
 
 endmodule															  // Fin del módulo
 		
