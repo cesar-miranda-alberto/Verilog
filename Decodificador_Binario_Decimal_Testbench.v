607824b707f9dbc8374eec49a8ac7f018da79de6
cesar-miranda-alberto <142947777+cesar-miranda-alberto@users.noreply.github.com>
2024-01-26 14:37:52 -0600

    Add files via upload

---
+++ Decodificador_Binario_Decimal_Testbench.v
@@ -0,0 +1,48 @@
+ /*
+				***** TESTBENCH DECODIFICADOR BINARIO DECIMAL *****
+				
+		El presente código implementa el desarrollo de un testbench para analizar el funcionamiento
+		de un decodificador binario a decimal cuyos vaalores serán exhibidos en un display 7 segmentos
+		de cátodo común a través de un FPGA de manera física, se hace esta mención ya que al momento 
+		de relizar el testbench los valores mostrados no serán los valores en decimal sino que serán
+		los valores necesarios para poder formar el número en el display 7 segmentos.
+		
+		Fecha: 21-Marzo-2023
+		Versión: 1.0
+		
+ */
+ 
+`timescale 1ns/100ps												// Establece la escala de tiempo
+ 
+ module Decodificador_Binario_Decimal_Testbench();					// Crea el módulo del testbench
+		reg [3:0] A;												// Declara la variable de entrada tipo reg de cuatro localidades
+		wire [6:0] S;												// Declara la variable de salida tipo wire de siete localidades
+		
+		Decodificador_Binario_Decimal tbd(A,S);						// Se hace la instanciación del módulo del decodificador
+		
+		initial 													// Inicio
+		begin
+			
+				A = 4'b0000;										// Numero 0
+				#100;
+				A = 4'b0001;										// Numero 1
+				#100;
+				A = 4'b0010;										// Numero 2
+				#100;
+				A = 4'b0011;										// Numero 3
+				#100;
+				A = 4'b0100; 										// Numero 4
+				#100;
+				A = 4'b0101;										// Numero 5
+				#100;
+				A = 4'b0110;										// Numero 6
+				#100;
+				A = 4'b0111;										// Numero 7
+				#100;
+				A = 4'b1000;										// Numero 8
+				#100;
+				A = 4'b1001;										// Numero 9
+				
+end																	// Fin
+
+endmodule 	
---
+++ Decodificador_Binario_Hexadecimal.v
@@ -0,0 +1,39 @@
+/*
+				***** DECODIFICADOR BINARIO DECIMAL *****
+				
+		El presente código desarrollado en verilog tiene como finalidad la implementación de 
+		un decodificador binario a hexadecimal donde el resultado será exhibido mediante un display
+		7 segmentos de cátodo común a través de un FPGA.
+		Fecha: 21-Marzo-2023
+		Versión: 1.0
+*/
+
+module Decodificador_Binario_Hexadecimal(							
+		input [3:0] A,										// Declaración de variable de entrada de cuatro bits
+		output reg [6:0] S);									// Declaración de variable de salida de siete bits
+
+		always @(A)
+		begin
+
+	case (A)												// Inicio del case con respecto a la entrada A
+		4'b0000 : S = 0000001;								// Numero 0
+		4'b0001 : S = 1001111;								// Numero 1
+		4'b0010 : S = 0010010;								// Numero 2
+		4'b0011 : S = 0000110;								// Numero 3
+		4'b0100 : S = 1001100;								// Numero 4
+		4'b0101 : S = 0100100;								// Numero 5
+		4'b0110 : S = 0100000;								// Numero 6
+		4'b0111 : S = 0001111;								// Numero 7
+		4'b1000 : S = 0000000; 								// Numero 8
+		4'b1001 : S = 0000100; 								// Numero 9
+		4'b1010 : S = 0001000;								// Letra A
+		4'b1011 : S = 1100000;								// Letra B
+		4'b1100 : S = 0110001;								// Letra C
+		4'b1101 : S	= 1000010;								//	Letra D
+		4'b1110 : S = 0110000;								// Letra E
+		4'b1111 : S = 0111000; 								// Letra F
+		default : S = 0000001;								// Salida por default
+	endcase														// Fin de los casos
+  end														
+
+endmodule		
