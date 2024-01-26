de7425105c7d8e6a6701feeda06e8c456a2cb64c
cesar-miranda-alberto <142947777+cesar-miranda-alberto@users.noreply.github.com>
2024-01-26 14:42:29 -0600

    Compuertas_Logicas.v

--- Compuertas_Logicas.v
+++ Compuertas_Logicas.v
@@ -7,27 +7,27 @@
 		Versión: 1.0
 */
 
-module Compuertas_Logicas(
-	input A,
-	input B,
-	output Snot,
-	output Sand,
-	output Sor,
-	output Sxor,
-	output Sxnor,
-	output Snand
+module Compuertas_Logicas(						// Creación del módulo
+	input A,							// Creación de variable de entrada A			
+	input B,							// Creación de variable de entrada B
+	output Snot,							// Creación de variable de salida compuerta NOT
+	output Sand,							// Creación de variable de salida compuerta AND
+	output Sor,							// Creación de variable de salida compuerta OR
+	output Sxor,							// Creación de variable de salida compuerta XOR
+	output Sxnor,							// Creación de variable de salida compuerta XNOR
+	output Snand							// Creación de variable de salida compuerta NAND
 	);
+		
+	not (Snot,A);							// Calcula el resultado de la compuerta NOT en la enttrada A y asigna el resultado a la variable de salida correspondiente
 	
-	not (Snot,A);
+	and (Sand,A,B);							// Calcula el resultado de la compuerta AND en la enttrada A y B despues asigna el resultado a la variable de salida correspondiente
 	
-	and (Sand,A,B);
+	or (Sor,A,B);							// Calcula el resultado de la compuerta OR en la enttrada A y B despues asigna el resultado a la variable de salida correspondiente
 	
-	or (Sor,A,B);
+	xor (Sxor,A,B);							// Calcula el resultado de la compuerta XOR en la enttrada A y B despues asigna el resultado a la variable de salida correspondiente
 	
-	xor (Sxor,A,B);
+	xnor (Sxnor,A,B);						// Calcula el resultado de la compuerta XNOR en la enttrada A y B despues asigna el resultado a la variable de salida correspondiente
 	
-	xnor (Sxnor,A,B);
+	nand (Snand,A,B);						// Calcula el resultado de la compuerta NAND en la enttrada A y B despues asigna el resultado a la variable de salida correspondiente
 	
-	nand (Snand,A,B);
-	
-endmodule 
+endmodule 								// Fin del módulo
