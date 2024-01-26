300049426b68e00c2658d534dd0d8309a0661164
cesar-miranda-alberto <142947777+cesar-miranda-alberto@users.noreply.github.com>
2024-01-26 14:48:36 -0600

    Decodificador_Binario_Decimal.v

--- Decodificador_Binario_Decimal.v
+++ Decodificador_Binario_Decimal.v
@@ -1,14 +1,23 @@
-module Decodificador_Binario_Decimal(A,S);	
+/*
+				***** DECODIFICADOR BINARIO DECIMAL *****
+				
+		El presente código desarrollado en verilog tiene como finalidad la implementación de 
+		un decodificador binario a decimal donde el resultado será exhibido mediante un display
+		7 segmentos de cátodo común a través de un FPGA.
+		Fecha: 21-Marzo-2023
+		Versión: 1.0
+*/
+
+module Decodificador_Binario_Decimal(A,S);								// Creación del módulo
 
 		
-	input [3:0]A;
-	output reg [6:0]S;		
-												// Declaración de variable de salida de siete bits
+	input [3:0]A;											// Declaración de variable de entrada de cuatro bits
+	output reg [6:0]S;										// Declaración de variable de salida de siete bits correspondiente al display siete segmentos
 
-	always @(A)
+	always @(A)											// Evalua la entrada A
 	begin
 	
-	case (A)																// Inicio del case con respecto a la entrada A
+	case (A)											// Inicio del case con respecto a la entrada A
 		4'b0000 :  S =7'b0000001; 								// Numero 0
 		4'b0001 :  S =7'b1001111; 								// Numero 1
 		4'b0010 :  S =7'b0010010; 								// Numero 2
@@ -21,8 +30,8 @@ module Decodificador_Binario_Decimal(A,S);
 		4'b1001 :  S =7'b0000100;  								// Numero 9
 		default :  S =7'b0000001; 								// Salida por default
 		
-	endcase																// Fin de los casos
+	endcase												// Fin de los casos
   
   end
 
-endmodule	
+endmodule												// Fin del módulo
