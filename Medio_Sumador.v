9dfd863d81df5bd315ddf13fd4440fe6e893b3fe
cesar-miranda-alberto <142947777+cesar-miranda-alberto@users.noreply.github.com>
2024-01-26 14:52:02 -0600

    Medio_Sumador.v

--- Medio_Sumador.v
+++ Medio_Sumador.v
@@ -8,13 +8,13 @@
 		
 */
 
-module medio_sumador(						// Creación y asignación de nombre al módulo
+module medio_sumador(								// Creación y asignación de nombre al módulo
 	input A,								// Declaración de variable de entrada A
 	input B,								// Declaración de variable de entrada B	
 	output Sa,								// Declaración de variable de salida 
 	output Cs);								// Declaración de variable de acarreo de salida
 	
-	xor (S,A,B);							// Se ejecuta una compuerta xor entre las entradas A y B cuyo resultado es asignado a la salida
-	and (Cs A,B);							// Se ejecuta una compuerta and entre las entradas A y B cuyo resultado es asignado al acarreo de salida
+	xor (S,A,B);								// Se ejecuta una compuerta xor entre las entradas A y B cuyo resultado es asignado a la salida
+	and (Cs A,B);								// Se ejecuta una compuerta and entre las entradas A y B cuyo resultado es asignado al acarreo de salida
 	
 endmodule									// Fin del módulo
