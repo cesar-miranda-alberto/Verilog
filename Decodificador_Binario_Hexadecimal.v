bfe37eb7e81cd7682b5e6a579741a9ebc9ff2de3
cesar-miranda-alberto <142947777+cesar-miranda-alberto@users.noreply.github.com>
2024-01-26 14:50:05 -0600

    Decodificador_Binario_Hexadecimal.v

--- Decodificador_Binario_Hexadecimal.v
+++ Decodificador_Binario_Hexadecimal.v
@@ -1,5 +1,5 @@
 /*
-				***** DECODIFICADOR BINARIO DECIMAL *****
+				***** DECODIFICADOR BINARIO HEXADECIMAL *****
 				
 		El presente código desarrollado en verilog tiene como finalidad la implementación de 
 		un decodificador binario a hexadecimal donde el resultado será exhibido mediante un display
@@ -9,13 +9,13 @@
 */
 
 module Decodificador_Binario_Hexadecimal(							
-		input [3:0] A,										// Declaración de variable de entrada de cuatro bits
-		output reg [6:0] S);									// Declaración de variable de salida de siete bits
+		input [3:0] A,									// Declaración de variable de entrada de cuatro bits
+		output reg [6:0] S);								// Declaración de variable de salida de siete bits
 
 		always @(A)
 		begin
 
-	case (A)												// Inicio del case con respecto a la entrada A
+	case (A)										// Inicio del case con respecto a la entrada A
 		4'b0000 : S = 0000001;								// Numero 0
 		4'b0001 : S = 1001111;								// Numero 1
 		4'b0010 : S = 0010010;								// Numero 2
@@ -29,11 +29,11 @@ module Decodificador_Binario_Hexadecimal(
 		4'b1010 : S = 0001000;								// Letra A
 		4'b1011 : S = 1100000;								// Letra B
 		4'b1100 : S = 0110001;								// Letra C
-		4'b1101 : S	= 1000010;								//	Letra D
+		4'b1101 : S = 1000010;								// Letra D
 		4'b1110 : S = 0110000;								// Letra E
 		4'b1111 : S = 0111000; 								// Letra F
 		default : S = 0000001;								// Salida por default
-	endcase														// Fin de los casos
-  end														
+	endcase											// Fin de los casos
+  end												// Fin												
 
-endmodule		
+endmodule											// Fin del módulo
